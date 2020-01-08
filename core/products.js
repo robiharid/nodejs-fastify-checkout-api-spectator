46a18392b12a49ea473f34e6b9f66cf0e1f6205c
robiharid <robiharid@gmail.com>
2020-01-08 00:46:03 +0000

    Map query params to shop products

---
+++ core/products.js
@@ -0,0 +1,8 @@
+const PRODUCTS = {
+  soup: 0.65,
+  bread: 0.8,
+  milk: 1.15,
+  apples: 1.0
+};
+
+module.exports = PRODUCTS;
--- routes/checkout.js
+++ routes/checkout.js
@@ -1,12 +1,19 @@
+const PRODUCTS = require('../core/products');
 async function routes(fastify, options) {
   // http://localhost:5000/checkout?items=apple&items=banana&currency=gbp
   const CURRENT_RATE = await require('../utils/currency')();
+  const USD = 1.0;
   const EUR = CURRENT_RATE.EUR;
   const GBP = CURRENT_RATE.GBP;
 
   fastify.get('/checkout', async (req, res) => {
+    // let userItems = JSON.parse(req.query.items);
+    const mappedItems = req.query.items.map(
+      item => PRODUCTS[item.toLowerCase()]
+    );
+
     res.send({
-      // items: req.query.items,
+      items: mappedItems,
       subtotal: 2.43,
       discounts: ['Apples 10% off'],
       discountAmt: 0.09,
