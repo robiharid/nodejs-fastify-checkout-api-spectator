47a19f2e8d9d0344eb03919121bd924f67a98178
robiharid <robiharid@gmail.com>
2020-01-08 01:07:30 +0000

    Return sum in correct currency (without discounts)

--- core/products.js
+++ core/products.js
@@ -1,8 +1,8 @@
 const PRODUCTS = {
-  soup: 0.65,
-  bread: 0.8,
-  milk: 1.15,
-  apples: 1.0
+  soup: 65,
+  bread: 80,
+  milk: 115,
+  apples: 100
 };
 
 module.exports = PRODUCTS;
--- routes/checkout.js
+++ routes/checkout.js
@@ -1,20 +1,28 @@
 const PRODUCTS = require('../core/products');
 async function routes(fastify, options) {
-  // http://localhost:5000/checkout?items=apple&items=banana&currency=gbp
+  //http://localhost:5000/checkout?items=Apples&items=Apples&items=Apples&items=Soup&currency=gbp
   const CURRENT_RATE = await require('../utils/currency')();
-  const USD = 1.0;
+  const USD = 1;
   const EUR = CURRENT_RATE.EUR;
   const GBP = CURRENT_RATE.GBP;
 
   fastify.get('/checkout', async (req, res) => {
-    // let userItems = JSON.parse(req.query.items);
+    const USER_CURRENCY = req.query.currency;
+
     const mappedItems = req.query.items.map(
       item => PRODUCTS[item.toLowerCase()]
     );
+    console.table(mappedItems);
+
+    const reducer = (acc, curr) => acc + curr;
+    const sum = (
+      (mappedItems.reduce(reducer) * req.query.currency) /
+      100
+    ).toFixed(2);
 
     res.send({
       items: mappedItems,
-      subtotal: 2.43,
+      subtotal: sum,
       discounts: ['Apples 10% off'],
       discountAmt: 0.09,
       total: EUR,
